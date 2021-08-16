9871f9040100fc2f48bd7fae57d3921f9cb3c626
eons <eons44@users.noreply.github.com>
2021-08-16 08:50:18 -0700

    Initializes project

---
+++ src/data/Entity.py
@@ -0,0 +1,13 @@
+import esam, eons
+
+class Entity(esam.Datum):
+    def __init__(self, id=0, name=eons.INVALID_NAME()):
+        super().__init__(name)
+        self.uniqueId = id
+        self.image_url = ""
+        self.contact_url = ""
+        self.description = ""
+        self.activities = []
+        self.interests = []
+        self.solicitations = []
+        self.relationships = {}
